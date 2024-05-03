b1008464ac1d2d8b38d172f365e06287ecb07b9a
Kurosu9 <kurstan.abazbekov@alatoo.edu.kg>
2024-05-03 17:37:32 +0600

    docs

--- store/urls.py
+++ store/urls.py
@@ -18,6 +18,18 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="YouTube API Documentation",
+        default_version='v1',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/", include("user.urls")),
@@ -25,7 +37,7 @@ urlpatterns = [
     path("short/", include("shorts.urls")),
     path("comment/", include("comment.urls")),
     path("channel/", include("channel.urls")),
-
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
 ]
 
 from django.conf.urls.static import static
